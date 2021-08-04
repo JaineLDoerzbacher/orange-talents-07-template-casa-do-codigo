c36c854dd2b79b7fbf9d6a1364903682d9637983
Jaine Doerzbacher <doerzbacherjaine@gmail.com>
2021-08-04 02:13:29 -0300

    Página detalhe

---
+++ src/main/java/casadocodigo/casadocodigo/dto/DetalheSiteAutorDTO.java
@@ -0,0 +1,24 @@
+package casadocodigo.casadocodigo.dto;
+
+import casadocodigo.casadocodigo.entities.Autor;
+
+public class DetalheSiteAutorDTO {
+
+    private String nomeAutor;
+
+    private String descAutor;
+
+    public DetalheSiteAutorDTO(Autor autor) {
+
+        nomeAutor = autor.getNomeAutor();
+        descAutor = autor.getDescricao();
+    }
+
+    public String getNomeAutor() {
+        return nomeAutor;
+    }
+
+    public String getDescAutor() {
+        return descAutor;
+    }
+}
