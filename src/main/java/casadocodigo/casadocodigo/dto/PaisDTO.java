bcb1ee57121ac5874fdc70cb92ac4e04b60ba4e6
Jaine Doerzbacher <doerzbacherjaine@gmail.com>
2021-08-04 02:26:17 -0300

    Cadastro de estado e pais

---
+++ src/main/java/casadocodigo/casadocodigo/dto/PaisDTO.java
@@ -0,0 +1,23 @@
+package casadocodigo.casadocodigo.dto;
+
+import casadocodigo.casadocodigo.entities.Pais;
+import casadocodigo.casadocodigo.util.validators.UniqueValue;
+
+import javax.validation.constraints.NotBlank;
+
+public class PaisDTO {
+
+    @NotBlank
+    @UniqueValue(domainClass = Pais.class, fieldName = "nomePais")
+    private String nomePais;
+
+
+
+    public void setNomePais(String nomePais) {
+        this.nomePais = nomePais;
+    }
+
+    public String getNomePais() {
+        return nomePais;
+    }
+}
